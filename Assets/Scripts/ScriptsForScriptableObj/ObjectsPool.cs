851832cfcf15bd04d9705628d265fb4ee735e0c3
kor1b <39777055+kor1b@users.noreply.github.com>
2020-02-02 00:38:44 +0200

    Hold aim in radius, objects pool

---
+++ Assets/Scripts/ScriptsForScriptableObj/ObjectsPool.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu (menuName = "ObjectsPool", fileName = "New Pool", order = 57)]
+public class ObjectsPool : ScriptableObject
+{
+    [SerializeField] private GameObject prefab;
+
+    [HideInInspector] public Transform parent;
+
+    [HideInInspector] public List<GameObject> objList = new List<GameObject> ();
+
+    public GameObject GetObject()
+    {
+        GameObject newObj = FindInactive (objList); //ищем доступный для переиспользования объект
+
+        if (newObj == null) //если таких нет
+        {
+            newObj = Instantiate (prefab);
+            // objList.Add(newObj);
+        }
+
+        newObj.transform.SetParent (parent);
+        objList.Add (newObj);
+
+        return newObj;
+    }
+
+    private GameObject FindInactive (List<GameObject> list) //ищем доступный для переиспользования объект
+    {
+        for (int i = 0; i < list.Count; i++)
+        {
+            if (!list[i].activeInHierarchy)
+            {
+                list[i].SetActive (true);
+
+                return list[i];
+            }
+        }
+
+        return null;
+    }
+}
--- Assets/Scripts/TrapSkill.cs
+++ Assets/Scripts/TrapSkill.cs
@@ -5,58 +5,70 @@ public class TrapSkill : MonoBehaviour
     [SerializeField] private TrapData trapData;
 
     [SerializeField] private GameObject trapPrefab;
+    [SerializeField] private CircleCollider2D trapSkillCollider;
     [SerializeField] private Transform trapsHolder;
 
-    private GameObject aimTrapPrefab;
-    private GameObject aimTrap;
-    private Transform aimsHolder;
+    [SerializeField] private ObjectsPool _objectsPool;
 
-    private Camera mainCamera;
+    private float _trapSkillRadius;
+    
+    private GameObject _aimTrapPrefab;
+    private GameObject _aimTrap;
+    // private Transform _aimsHolder;
 
-   [SerializeField] private bool _aimed = false;
+    private Camera _mainCamera;
+
+    private bool _aimed = false;
 
     private void Awake()
     {
         Init();
 
-        aimTrap = Instantiate (aimTrapPrefab, Vector2.zero, Quaternion.identity, aimsHolder);
-        aimTrap.SetActive (false);
+        _aimTrap = Instantiate (_aimTrapPrefab, Vector2.zero, Quaternion.identity, trapsHolder);
+        _aimTrap.SetActive (false);
     }
 
     private void Init()
     {
-        aimTrapPrefab = trapData.AimTrap;
-        mainCamera = Camera.main;
+        _aimTrapPrefab = trapData.AimTrap;
+        _mainCamera = Camera.main;
+        _trapSkillRadius = trapSkillCollider.radius;
+        
+        _objectsPool.parent = trapsHolder;
+        _objectsPool.objList.Clear();
     }
 
     private void Aim()
     {
-        if (aimTrap.gameObject.activeSelf == false)
-            aimTrap.gameObject.SetActive (true);
+        if (_aimTrap.gameObject.activeSelf == false)
+            _aimTrap.gameObject.SetActive (true);
 
-        aimTrap.transform.position = GetMousePosition();
+        _aimTrap.transform.position = GetMousePosition();
     }
 
     //add it to 'OnRepairGunSkillCasted' event
     public void StopAim()
     {
-        aimTrap.SetActive (false);
+        _aimTrap.SetActive (false);
+        _aimed = false;
     }
 
     private void Skill()
     {
-        SpawnTrap (GetMousePosition());
+        SpawnTrap (_aimTrap.transform.position);
         StopAim();
     }
 
     private void SpawnTrap (Vector2 spawnPos)
     {
-        Instantiate (trapPrefab, spawnPos, Quaternion.identity, trapsHolder);
+        var obj= _objectsPool.GetObject();
+        obj.transform.position = spawnPos;
+
     }
 
     private Vector2 GetMousePosition()
     {
-        Vector3 aimPos = mainCamera.ScreenToWorldPoint (Input.mousePosition);
+        Vector3 aimPos = _mainCamera.ScreenToWorldPoint (Input.mousePosition);
         aimPos.z = 0;
 
         return aimPos;
@@ -64,19 +76,44 @@ public class TrapSkill : MonoBehaviour
 
     private void Update()
     {
+        //hold aim in trapSkillRadius
         if (_aimed)
-            aimTrap.transform.position = GetMousePosition();
+        {
+            _aimTrap.transform.position = GetMousePosition();
 
+            float distance = Vector2.Distance (_aimTrap.transform.position, transform.position);
+
+            if (distance > _trapSkillRadius)
+            {
+                Vector3 fromOriginToObject =
+                    _aimTrap.transform.localPosition - transform.localPosition; //~GreenPosition~ - *BlackCenter*
+               
+                fromOriginToObject *= _trapSkillRadius / distance;              //Multiply by trapSkillRadius //Divide by Distance
+
+                _aimTrap.transform.localPosition =
+                    transform.localPosition + fromOriginToObject; //*BlackCenter* + all that Math
+            }
+        }
+
+        //first click - aim
         if (Input.GetMouseButtonDown (1) && _aimed == false)
         {
             Aim();
             _aimed = true;
         }
 
+        //second click - spawn trap
         else if (Input.GetMouseButtonDown (1) && _aimed)
         {
             Skill();
             _aimed = false;
         }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+
+        Gizmos.DrawWireSphere (transform.position, _trapSkillRadius);
     }
 }

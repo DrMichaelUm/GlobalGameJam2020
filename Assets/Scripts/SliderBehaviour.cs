68884d55c867f99eab376ab07c9fcfeab1d54e5f
tardis2snej <47867092+tardis2snej@users.noreply.github.com>
2020-02-01 19:24:29 +0200

    slider

---
+++ Assets/Scripts/SliderBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderBehaviour : MonoBehaviour
+{
+    [SerializeField] private ScoreCounter scoreData;
+    private Slider _slider;
+
+    private void Awake()
+    {
+        scoreData.Score = 0;
+        _slider = GetComponent<Slider>();
+    }
+
+    public void OnScoreChanged()
+    {
+        _slider.value = scoreData.Score;
+    }
+}

b8c7be160efe623fb7d32d5da73dbe052e29ed90
tardis2snej <47867092+tardis2snej@users.noreply.github.com>
2020-02-01 20:41:55 +0200

    Timer + TMPro

---
+++ Assets/temp/TimerTest.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NaughtyAttributes;
+using UnityEngine;
+
+public class TimerTest : MonoBehaviour
+{
+    public GameEvent gameStart;
+    
+    [Button]
+    public void StartGame()
+    {
+        gameStart.Raise();
+    }
+}

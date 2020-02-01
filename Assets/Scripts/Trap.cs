d75eac2273c8784ce3ca4303216494e0c144fc00
kor1b <39777055+kor1b@users.noreply.github.com>
2020-02-01 19:52:24 +0200

    TrapSkill

---
+++ Assets/Scripts/Trap.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public class Trap : MonoBehaviour, IPlayable
+{
+   [SerializeField] private TrapData trapData;
+
+   [SerializeField] private GameEvent OnTrapSpawned;
+   [SerializeField] private GameEvent OnTrapDeactivated;
+   [SerializeField] private GameEvent OnDestroyerTrapped;
+
+   private float lifetime;
+   
+   private void Awake()
+   {
+      Init();
+   }
+
+   private void OnEnable()
+   {
+      //deactivate in 'lifetime' time after spawn
+      OnTrapSpawned.Raise();
+      StartCoroutine (Deactivate(lifetime));
+   }
+
+   private void Init()
+   {
+      lifetime = trapData.Lifetime;
+   }
+
+   private IEnumerator Deactivate(float time)
+   {
+      yield return new WaitForSeconds (time);
+      
+      OnTrapDeactivated.Raise();
+      gameObject.SetActive (false);
+   }
+
+   //set to 'OnDestroyerTrapped'
+   public void Skill()
+   {
+      //TODO EFFECT
+      Debug.Log ("TRAPPED");
+   }
+
+   private void OnTriggerEnter2D (Collider2D other)
+   {
+      if (other.CompareTag ("Destroyer"))
+         OnDestroyerTrapped.Raise();
+   }
+}
---
+++ Assets/Scripts/TrapSkill.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+
+public class TrapSkill : MonoBehaviour
+{
+    [SerializeField] private TrapData trapData;
+
+    [SerializeField] private GameObject trapPrefab;
+    [SerializeField] private Transform trapsHolder;
+
+    private GameObject aimTrapPrefab;
+    private GameObject aimTrap;
+    private Transform aimsHolder;
+
+    private Camera mainCamera;
+
+    private bool _aimed = false;
+
+    private void Awake()
+    {
+        Init();
+
+        aimTrap = Instantiate (aimTrapPrefab, Vector2.zero, Quaternion.identity, aimsHolder);
+        aimTrap.SetActive (false);
+    }
+
+    private void Init()
+    {
+        aimTrapPrefab = trapData.AimTrap;
+        mainCamera = Camera.main;
+    }
+
+    private void Aim()
+    {
+        if (aimTrap.gameObject.activeSelf == false)
+            aimTrap.gameObject.SetActive (true);
+
+        aimTrap.transform.position = GetMousePosition();
+    }
+
+    //add it to 'OnRepairGunSkillCasted' event
+    public void StopAim()
+    {
+        aimTrap.SetActive (false);
+    }
+
+    private void Skill()
+    {
+        SpawnTrap (GetMousePosition());
+        StopAim();
+    }
+
+    private void SpawnTrap (Vector2 spawnPos)
+    {
+        Instantiate (trapPrefab, spawnPos, Quaternion.identity, trapsHolder);
+    }
+
+    private Vector2 GetMousePosition()
+    {
+        Vector3 aimPos = mainCamera.ScreenToWorldPoint (Input.mousePosition);
+        aimPos.z = 0;
+
+        return aimPos;
+    }
+
+    private void Update()
+    {
+        if (_aimed)
+            aimTrap.transform.position = GetMousePosition();
+
+        if (Input.GetMouseButtonDown (1) && _aimed == false)
+        {
+            _aimed = true;
+            Aim();
+        }
+
+        else if (Input.GetMouseButtonDown (1) && _aimed)
+        {
+            Skill();
+            _aimed = false;
+        }
+    }
+}

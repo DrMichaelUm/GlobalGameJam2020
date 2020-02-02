42039b3625d71b101a5577158bda3b970f7a864a
kor1b <39777055+kor1b@users.noreply.github.com>
2020-02-02 13:16:55 +0200

    Trap layer, small fixes (lights ...)

--- Assets/Scripts/GunSkill.cs
+++ Assets/Scripts/GunSkill.cs
@@ -1,17 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class GunSkill : MonoBehaviour
 {
     [SerializeField] private GameEvent OnRepairGunSkillCasted;
+
+    [SerializeField] private UnityEvent OnShooted;
+    [SerializeField] private UnityEvent OnStoppedShooting;
+    
     private void Update()
     {
         if (Input.GetMouseButton(0))
             Skill();
+        
+        else if (Input.GetMouseButtonUp (0))
+            OnStoppedShooting.Invoke();
     }
     public void Skill()
     {
         OnRepairGunSkillCasted.Raise();
+        OnShooted.Invoke();
     }
 }

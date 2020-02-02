e3161236e74e8675286c20de7626cd55e50bc4ee
kor1b <39777055+kor1b@users.noreply.github.com>
2020-02-02 11:40:32 +0200

    Gun trails, player knock strength

--- Assets/Scripts/GunSkill.cs
+++ Assets/Scripts/GunSkill.cs
@@ -5,13 +5,19 @@ using UnityEngine;
 public class GunSkill : MonoBehaviour
 {
     [SerializeField] private GameEvent OnRepairGunSkillCasted;
+    [SerializeField] private ParticleSystem trailsGunParticles;
+    
     private void Update()
     {
         if (Input.GetMouseButton(0))
             Skill();
+        
+        else if (Input.GetMouseButtonUp (0))
+            trailsGunParticles.Stop();
     }
     public void Skill()
     {
         OnRepairGunSkillCasted.Raise();
+        trailsGunParticles.Play();
     }
 }

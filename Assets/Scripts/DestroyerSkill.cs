8b72922d81ad4f0198347ef214d687d7d2b343b9
MrMichaelUm <50107636+MrMichaelUm@users.noreply.github.com>
2020-02-05 16:02:23 +0200

    Scripts splitted

---
+++ Assets/Scripts/DestroyerSkill.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyerSkill : MonoBehaviour, IPlayable
+{
+    [SerializeField] private GameEvent OnDestroySkillCasted;
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Skill();
+        }
+    }
+    public void Skill()
+    {
+        //TODO Партикл и звук волны
+        OnDestroySkillCasted.Raise();
+    }
+}
